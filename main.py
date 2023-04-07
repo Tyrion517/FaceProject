59f87804f1e66c7de4553db22917593634c44923
Tyrion <tyrion@517mail.com>
2023-04-07 08:27:25 +0800

    nothing

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from take_photo import take_photo
 
 rec = recognition()
 # TODO: 下面两行仅仅为了调试 发布时应删除
-# print(f'pic_names: {rec.pic_names}')
-# print(f'known_names: {rec.known_names}')
+print(f'pic_names: {rec.pic_names}')
+print(f'known_names: {rec.known_names}')
 
 while True:
     if input('command:') == 'n':
@@ -23,4 +23,4 @@ while True:
             else:
                 print('Not registered!')
             # TODO： 下一行代码仅作调试用 发布时应删除
-            # print(f'distances: {face_recognition.face_distance(rec.known_face_encodings,unknown_face)}')
+            print(f'distances: {fr.face_distance(rec.known_face_encodings,unknown_face)}')
