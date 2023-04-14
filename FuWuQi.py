f1091b5153d7b0107517a6f5ac5929276b257d6b
Tyrion <tyrion@517mail.com>
2023-04-14 08:52:12 +0800

    UI系统

---
+++ FuWuQi.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+# import os
+from ftplib import FTP
+import datetime
+
+"""
+
+可以实现上传 下载单个文件
+
+"""
+
+
+def ftp_connect(host, port, username, password):
+    ftp = FTP()
+    # ftp.set_debuglevel(2)
+    ftp.connect(host, port)
+    ftp.login(username, password)
+    # ftp.set_pasv(False)
+    return ftp
+
+
+"""
+
+从ftp服务器下载文件
+
+remotepath:远程路径
+localpath：本地路径
+
+"""
+
+
+def download_file(ftp, remotepath, localpath):
+    bufsize = 1024
+    fp = open(localpath, 'wb')
+    ftp.retrbinary('RETR ' + remotepath, fp.write, bufsize)
+    ftp.set_debuglevel(0)
+    fp.close()
+
+
+"""
+
+从本地上传文件到ftp
+
+remotepath:远程路径
+localpath：本地路径
+
+"""
+
+
+def upload_file(ftp, remotepath, localpath):
+    bufsize = 1024
+    fp = open(localpath, 'rb')
+
+    ftp.storbinary('STOR ' + remotepath, fp, bufsize)
+    ftp.set_debuglevel(0)
+    fp.close()
+
+
+if __name__ == "__main__":
+    host = '192.168.1.XXX'  # IP
+    port = 21  # 端口
+    username = 'ftpadmin'  # 用户名
+    password = 'ftpadmin'  # 密码
+    ftp = ftp_connect(host, port, username, password)
+
+    upload_file(ftp, r"/CMAQ/ftp.log", r"/run/media/test/mydata/data_post/ftp.log")
+
+    download_file(ftp, r"/CMAQ/ftp.log", r"/run/media/test/mydata/data_post/ftp.log")
+
+    ftp.quit()
+
+
--- main.py
+++ main.py
@@ -1,26 +1,70 @@
-import sys
-
 import face_recognition as fr
+import cv2
+import os
 from recognition import recognition
 from take_photo import take_photo
+from welcome_phrase import welcome
 
+pic_path = os.path.join(os.path.dirname(__file__), 'pics')
 
-rec = recognition()
-# TODO: 下面两行仅仅为了调试 发布时应删除
-print(f'pic_names: {rec.pic_names}')
-print(f'known_names: {rec.known_names}')
+def cmd_recognize(rec: recognition):
+    try:
+        unknown_face = fr.face_encodings(take_photo())[0]  # 拍照 加载并编码人脸
+    except IndexError:
+        print('No face detected!!!')
+    else:
+        verified = rec.verify_face(unknown_face)
+        if verified:
+            print(verified)
+        else:
+            print('Not registered!')
+        # TODO： 下一行代码仅作调试用 发布时应删除
+        # print(f'distances: {fr.face_distance(rec.known_face_encodings, unknown_face)}')
 
-while True:
-    if input('command:') == 'n':
+def cmd_register(rec: recognition, password: str):
+    _password = input('Please enter the password:')
+    if _password == password:
         try:
-            unknown_face = fr.face_encodings(take_photo())[0]  # 拍照 加载并编码人脸
+            new_face = take_photo()
+            unknown_face = fr.face_encodings(new_face)[0]  # 拍照 加载并编码人脸
         except IndexError:
             print('No face detected!!!')
         else:
             verified = rec.verify_face(unknown_face)
             if verified:
-                print(verified)
+                print(f'Already registered, {verified}!')
+                # TODO： 下一行代码仅作调试用 发布时应删除
+                print(f'distances: {fr.face_distance(rec.known_face_encodings, unknown_face)}')
             else:
-                print('Not registered!')
-            # TODO： 下一行代码仅作调试用 发布时应删除
-            print(f'distances: {fr.face_distance(rec.known_face_encodings,unknown_face)}')
+                name = input('Please enter your name:')
+                _ = cv2.imwrite(f'{pic_path}/known_faces/{name}.jpg', cv2.cvtColor(new_face, cv2.COLOR_RGB2BGR))
+                if _:
+                    print('successfully registered! Use "refresh" to reload')
+    else:
+        print('Wrong password')
+
+
+if __name__ == '__main__':
+    rec = recognition()
+    # TODO: 下面两行仅仅为了调试 发布时应删除
+    # print(f'pic_names: {rec.pic_names}')
+    # print(f'known_names: {rec.known_names}')
+
+    while True:
+        command = input('command:')
+        if command == 'recognize':
+            # 识别人脸
+            cmd_recognize(rec)
+
+        elif command == 'register':
+            # 注册人脸 要求输入密码
+            cmd_register(rec, rec.password)
+
+        elif command == 'refresh':
+            # 重新加载人脸
+            rec = recognition()
+        else:
+            print(f'Command {command} not found')
+
+
+
